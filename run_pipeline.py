9e8dff70fc70e075714f0fa66014512e57c3d585
Kushal Atul Ramaiya <kushalramaiya@gmail.com>
2025-03-24 21:35:18 +0530

    Added mlflow ui

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,6 +1,14 @@
 from pipelines.training_pipelines import train_pipeline
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
 
 if __name__ == "__main__":
     train_pipeline(
         data_path="data/olist_customers_dataset.csv"
+    )
+    print(
+        "Now run \n "
+        f"    mlflow ui --backend-store-uri '{get_tracking_uri()}'\n"
+        "To inspect your experiment runs within the mlflow UI.\n"
+        "You can find your runs tracked within the `mlflow_example_pipeline`"
+        "experiment. Here you'll also be able to compare the two runs.)"
     )
